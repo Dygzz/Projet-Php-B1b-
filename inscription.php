7c01b1f89ddafba45741e8d091b1c78875c3bcc3
Ilias <mi.souidi@gmail.com>
2017-02-14 11:36:57 +0100

    inscription finis

---
+++ inscription.php
@@ -0,0 +1,41 @@
+<?php
+
+$dsn = 'mysql:dbname=projet.php;host:127.0.0.1';
+$user = 'root';
+$password = '';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+if (!empty($_POST)) {
+    $stmt = $dbh->prepare('
+                INSERT INTO users(pseudo, email, password) 
+                VALUES (:pseudo, :email, :password);');
+    $stmt->execute([
+        ':pseudo' => $_POST['name'],
+        ':email' => $_POST['email'],
+        ':password' => $_POST['password']
+    ]);
+}
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+        <form action="" method="post">
+            <input type="text" name="name" id="">
+            <input type="text" name="email" id="">
+            <input type="text" name="password" id="">
+            <button type="submit">Enregistrer</button>
+        </form>
+    </body>
+</html>
+
