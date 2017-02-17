0dfd18c5a8965dffff0b20941bb322faffd6fcbb
Ilias <mi.souidi@gmail.com>
2017-02-17 18:07:24 +0100

    Finale Inscription

--- inscription.php
+++ inscription.php
@@ -33,30 +33,131 @@ elseif (count($result) == 0) {
         ':password' => $hashed_password
     ]);
 
+    
     }
 }
 
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="./styles/inscritpion.css" />
+        <link href="https://fonts.googleapis.com/css?family=Sansita" rel="stylesheet">
+
     </head>
     <body>
-        <form action="" method="post">
-            <label for="name">Pseudo</label>
-            <input type="text" name="name" id="">
-            <label for="email">Email</label>
-            <input type="text" name="email" id="">
-            <label for="password">Mot de Passe</label>
-            <input type="text" name="password" id="">
-            <button type="submit">Inscription</button>
-        </form>
+        <div>
+            <div id="first">
+                <div class="container">
+
+                    <img src="ImagesHtml/upload.png" class="logo" class="img-responsive">
+
+
+                    <div>
+                        <script language="javascript">
+                            function date_heure(id)
+                            {
+                                date = new Date;
+                                annee = date.getFullYear();
+                                moi = date.getMonth();
+                                mois = new Array('janvier', 'f&eacute;vrier', 'mars', 'avril', 'mai', 'juin', 'juillet', 'Ao&ucirc;t', 'septembre', 'octobre', 'novembre', 'D&eacute;cembre');
+                                j = date.getDate();
+                                jour = date.getDay();
+                                jours = new Array('dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi');
+                                h = date.getHours();
+                                if(h<10)
+                                {
+                                    h = "0"+h;
+                                }
+                                m = date.getMinutes();
+                                if(m<10)
+                                {
+                                    m = "0"+m;
+                                }
+                                s = date.getSeconds();
+                                if(s<10)
+                                {
+                                    s = "0"+s;
+                                }
+                                resultat = 'Nous sommes le '+jours[jour]+' '+j+' '+mois[moi]+' '+annee+' et il est '+h+':'+m+':'+s;
+                                document.getElementById(id).innerHTML = resultat;
+                                setTimeout('date_heure("'+id+'");','1000');
+                                return true;
+                            }
+                        </script>
+                        <center><span id="date_heure"></span></center>
+                        <script type="text/javascript">window.onload = date_heure('date_heure');</script>
+
+                    </div>
+                </div>
+            </div>
+
+            <div id="second" class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-4 col-md-offset-4">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <strong> Inscrivez-vous !</strong>
+                            </div>
+                            <div class="panel-body">
+                                <form role="form" method="post">
+                                    <fieldset>
+                                        <div class="row">
+                                            <div class="center-block">
+                                                <img class="profile-img"
+                                                     src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120" alt="">
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-sm-12 col-md-10  col-md-offset-1 ">
+
+                                                <div class="form-group">
+                                                    <div class="input-group">
+                                                        <span class="input-group-addon">
+                                                            <i class="glyphicon glyphicon-user"></i>
+                                                        </span> 
+                                                        <input class="form-control" placeholder="name" name="name" type="text" autofocus>
+                                                    </div>
+                                                </div>
+
+
+                                                <div class="form-group">
+                                                    <div class="input-group">
+                                                        <span class="input-group-addon">
+                                                            <i class="glyphicon glyphicon-user"></i>
+                                                        </span> 
+                                                        <input class="form-control" placeholder="Email" name="email" type="text" autofocus>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <div class="input-group">
+                                                        <span class="input-group-addon">
+                                                            <i class="glyphicon glyphicon-lock"></i>
+                                                        </span>
+                                                        <input class="form-control" placeholder="Mot de passe" name="password" type="password" value="">
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <input type="submit" class="btn btn-lg btn-primary btn-block" value="Inscription" >
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </fieldset>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h4 id="end">Si vous avez déjà un compte : <a href="./Connection.php" ><strong>Cliquez-ici</strong></a></h3>
+        </div>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </body>
 </html>
-
