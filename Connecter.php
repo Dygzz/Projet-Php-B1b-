6e01f7303d8b6b6a8d0c5947e284b63393ad0248
Diego Le Hen <Diego Le Hen>
2017-02-19 20:34:23 +0100

    petit erreure

--- Connecter.php
+++ Connecter.php
@@ -2,17 +2,15 @@
 
 require 'connect.php';
 
-$stmt = $dbh->prepare('SELECT NAME 
-                       FROM pictures
-                       ORDER  by id DESC LIMIT 0, 5  
-                       '
-);
-$stmt->execute();
-$lastPicture = $stmt->fetchall();
-
-for ($i = 0; $i < count($lastPicture); $i ++) {
-
-    echo '<img src="Images/' .$lastPicture[$i][0] .'">';
+if (!empty($_SESSION['id'])) {
+    $stmt = $dbh->prepare('SELECT *
+                       FROM users
+                       WHERE id = :id
+                    ');
+    $stmt->execute([
+        ':id' => $_SESSION['id']
+    ]);
+    $user = $stmt->fetchAll();
 }
 
 $pixName = '';
@@ -60,21 +58,165 @@ if (!empty($_FILES['picture']) && $_FILES['picture']['error'] == 0) {
         echo 'Erreur de format';
     }
 }?>
-<?php
-var_dump($_SESSION);
-if(empty($_SESSION['id'])) {
-    echo '<a href="Connection.php">log</a>
-          <a href="inscription.php">register</a>';
-}
-else{
-    echo '<a href="Profil.php">profil</a>';
-}
-?>
-<form action="" method="post" enctype="multipart/form-data">
-    <input type="file" name="picture" accept="image/*" id="">
-    <button type="submit">
-        Upload
-    </button>
-</form>
 
-<button><a href="Deconexion.php">Deconnexion</a></button>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="styles/styles1.css" />
+
+</head>
+<body>
+<div id="first">
+    <div class="container">
+        <div class="col-md-4 col-sm-4 col-xs-4" align="left">
+            <a href="Connecter.php">
+                <img src="ImagesHtml/upload.png" class="logo" class="img-responsive">
+            </a>
+
+        </div>
+
+        <div class="col-md-4 col-sm-4 col-xs-4" align="center">
+            <script language="javascript">
+                function date_heure(id)
+                {
+                    date = new Date;
+                    annee = date.getFullYear();
+                    moi = date.getMonth();
+                    mois = new Array('janvier', 'f&eacute;vrier', 'mars', 'avril', 'mai', 'juin', 'juillet', 'Ao&ucirc;t', 'septembre', 'octobre', 'novembre', 'D&eacute;cembre');
+                    j = date.getDate();
+                    jour = date.getDay();
+                    jours = new Array('dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi');
+                    h = date.getHours();
+                    if(h<10)
+                    {
+                        h = "0"+h;
+                    }
+                    m = date.getMinutes();
+                    if(m<10)
+                    {
+                        m = "0"+m;
+                    }
+                    s = date.getSeconds();
+                    if(s<10)
+                    {
+                        s = "0"+s;
+                    }
+                    resultat = 'Nous sommes le '+jours[jour]+' '+j+' '+mois[moi]+' '+annee+' et il est '+h+':'+m+':'+s;
+                    document.getElementById(id).innerHTML = resultat;
+                    setTimeout('date_heure("'+id+'");','1000');
+                    return true;
+                }
+            </script>
+            <center><span id="date_heure"></span></center>
+            <script type="text/javascript">window.onload = date_heure('date_heure');</script>
+
+        </div>
+
+        <?php
+
+        if(empty($_SESSION['id'])) {
+            echo '  <div class="col-md-2 col-sm-2 col-xs-2 logreg"  >
+                        <a href="inscription.php" >Inscription </a>
+                       
+                       </div>
+                       
+                       <div class="col-md-2 col-sm-2 col-xs-2 logreg"  >
+                       <a href="Connection.php">Connection</a>
+                         </div>'
+            ;
+        }
+        else {
+            ?>
+            <div class="col-md-4 col-sm-4 col-xs-4" align="right" >
+
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <span class="glyphicon glyphicon-user"></span>
+                        <strong>Compte</strong>
+                        <span class="glyphicon glyphicon-chevron-down"></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <div class="navbar-login">
+                                <div class="row">
+                                    <div class="col-lg-8">
+                                        <p class="text-left"><strong>Pseudo: </strong><?= htmlentities($user[0]['Pseudo']) ?></p>
+                                        <p class="text-left small">Email: <?= htmlentities($user[0]['Email']) ?></p>
+                                        <p class="text-left">
+                                            <a href="Modifprofil.php" class="btn btn-primary btn-block btn-sm">Modification</a>
+                                            <a href="Profil.php" class="btn btn-primary btn-block btn-sm">Profil</a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <div class="navbar-login navbar-login-session">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <a href="Deconexion.php" class="btn btn-danger btn-block">Deconnexion</a>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+            </div>
+            <?php
+        }
+        ?>
+    </div>
+</div>
+<div id="second">
+    <h1>Derniere Image Upload:</h1>
+
+    <hr>
+
+    <?php
+    $stmt = $dbh->prepare('SELECT NAME 
+                       FROM pictures
+                       ORDER  by id DESC LIMIT 0, 5  
+                       '
+    );
+    $stmt->execute();
+    $lastPicture = $stmt->fetchall();
+
+    for ($i = 0; $i < count($lastPicture); $i ++) {
+
+        echo'
+
+        <div class="col-md-3  col-md-offset-1 image">
+            <img src="Images/' .$lastPicture[$i][0] .'" class="img-responsive">
+        </div>';
+    }
+    ?>
+
+    <div class="clearfix"></div><br>
+</div>
+
+<div id="third">
+
+
+    <div class="row">
+        <div class="pulse-btn" >
+            <div class="container">
+                <form action="" method="post" enctype="multipart/form-data">
+                    <input type="file" name="picture" accept="image/*" id="">
+                    <button type="submit" class="btn btn-primary">Upload</button>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed --><!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
--- Profil.php
+++ Profil.php
@@ -15,70 +15,134 @@ $stmt->execute([
 ]);
 $user = $stmt->fetchAll();
 
-echo 'Le pseudo est : ' . htmlentities($user[0]['Pseudo']) . '<br>';
-echo 'L\'email est : ' . htmlentities($user[0]['Email']) . '<br>';
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="styles/styles1.css" />
 
-if (!empty($_POST)){
+</head>
+<body>
+<div id="first">
+    <div class="container">
+        <div class="col-md-4 col-sm-4 col-xs-4" align="left">
+            <a >
+                <img src="ImagesHtml/upload.png" class="logo" class="img-responsive">
+            </a>
 
-    $hashed_password = crypt($_POST['password'], '_J9..rasm');
+        </div>
 
-    $stmt = $dbh->prepare ('UPDATE users 
-                            SET Pseudo = :name, Email = :email, password = :password
-                            where id = :id');
-    $stmt->execute([
-        ':id' => $user[0]['id'],
-        ':email' =>$_POST['Email'],
-        ':name' =>$_POST['Pseudo'],
-        ':password' => $hashed_password
-    ]);
-    $stmt->fetchAll();
-    $user[0]['Pseudo'] = $_POST['Pseudo'];
-    $user[0]['Email'] = $_POST['Email'];
-    echo 'information modifiée' ;
-    header('Location: ./Profil.php');
-}
+        <div class="col-md-4 col-sm-4 col-xs-4" align="center">
+            <script language="javascript">
+                function date_heure(id)
+                {
+                    date = new Date;
+                    annee = date.getFullYear();
+                    moi = date.getMonth();
+                    mois = new Array('janvier', 'f&eacute;vrier', 'mars', 'avril', 'mai', 'juin', 'juillet', 'Ao&ucirc;t', 'septembre', 'octobre', 'novembre', 'D&eacute;cembre');
+                    j = date.getDate();
+                    jour = date.getDay();
+                    jours = new Array('dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi');
+                    h = date.getHours();
+                    if(h<10)
+                    {
+                        h = "0"+h;
+                    }
+                    m = date.getMinutes();
+                    if(m<10)
+                    {
+                        m = "0"+m;
+                    }
+                    s = date.getSeconds();
+                    if(s<10)
+                    {
+                        s = "0"+s;
+                    }
+                    resultat = 'Nous sommes le '+jours[jour]+' '+j+' '+mois[moi]+' '+annee+' et il est '+h+':'+m+':'+s;
+                    document.getElementById(id).innerHTML = resultat;
+                    setTimeout('date_heure("'+id+'");','1000');
+                    return true;
+                }
+            </script>
+            <center><span id="date_heure"></span></center>
+            <script type="text/javascript">window.onload = date_heure('date_heure');</script>
 
-$stmt = $dbh->prepare('SELECT Name
+        </div>
+
+        <div class="col-md-4 col-sm-4 col-xs-4" align="right" >
+
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                    <span class="glyphicon glyphicon-user"></span>
+                    <strong>Compte</strong>
+                    <span class="glyphicon glyphicon-chevron-down"></span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li>
+                        <div class="navbar-login">
+                            <div class="row">
+                                <div class="col-lg-8">
+                                    <p class="text-left"><strong>Pseudo: </strong><?= htmlentities($user[0]['Pseudo']) ?></p>
+                                    <p class="text-left small">Email: <?= htmlentities($user[0]['Email']) ?></p>
+                                    <p class="text-left">
+                                        <a href="Modifprofil.php" class="btn btn-primary btn-block btn-sm">Modification</a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="divider"></li>
+                    <li>
+                        <div class="navbar-login navbar-login-session">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <a href="Deconexion.php" class="btn btn-danger btn-block">Deconnexion</a>
+
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </li>
+        </div>
+
+    </div>
+</div>
+<div id="second">
+    <h1>Les Image que vous avez Upload:</h1>
+
+    <hr>
+
+    <?php
+    $stmt = $dbh->prepare('SELECT Name
                        FROM pictures p, users u
                        WHERE p.id_user = u.id
                        AND :id = u.id 
                     ');
-$stmt->execute([
-    ':id' => $_SESSION['id']
-]);
-$picture = $stmt->fetchAll();
+    $stmt->execute([
+        ':id' => $_SESSION['id']
+    ]);
+    $picture = $stmt->fetchAll();
+
+    for ($i = 0; $i < count($picture); $i ++) {
+
+        echo'
+
+        <div class="col-md-3  col-md-offset-1 image">
+           <img src="Images/' .$picture[$i][0] .'">
+        </div>';
+    }
+    ?>
+
+    <div class="clearfix"></div><br>
+</div>
 
 
 
-for ($i = 0; $i < count($picture); $i ++) {
-
-    echo '<img src="Images/' .$picture[$i][0] .'">';
-}
-?>
-
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<form action="" method="post">
-    <label for="Nom">Pseudo :</label>
-    <input type="text" name="Pseudo" value="<?= htmlentities($user[0]['Pseudo']) ?> " >
-    <br>
-    <label for="Email">L'Email:</label>
-    <input type="text" name="Email" value="<?= htmlentities($user[0]['Email']) ?>">
-
-    <br>
-    <label for="Email">Modifier le password</label>
-    <input type="text" name="password" >
-    <button type="submit" >Valider</button>
-</form>
-
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <button><a href="./Modifprofil.php">modifier</a></button>
 </body>
